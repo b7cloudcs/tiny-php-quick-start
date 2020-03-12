d4952f553a4504a399d02520867eec044c0a4e23
dialogueplay <59691074+dialogueplay@users.noreply.github.com>
2020-03-12 13:50:30 +0800

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -10,25 +10,39 @@ require __DIR__.'/vendor/autoload.php';
 
 // Helpers
 // https://github.com/dialogueplay/php-request-helper
-// use PhpRequestHelper\Helper;
-// use PhpRequestHelper\Request;
-// use PhpRequestHelper\Response;
+class Helper extends PhpRequestHelper\Helper{}
+class Request extends PhpRequestHelper\Request{}
+class Response extends PhpRequestHelper\Response{}
 
 // Assert
 // https://github.com/webmozart/assert
-// use Webmozart\Assert\Assert;
+class Assert extends Webmozart\Assert\Assert{
+
+    // my rules
+    public static function __callStatic($name, $arguments)
+    {
+        // 中国电话号码 1qqwwwweeee
+        if($name == "chineseCellphone")
+        {
+            if(!\preg_match('/^1[0-9]{10}$/', $arguments[0])){
+                static::reportInvalidArgument(\sprintf(
+                    $arguments[1] ?: 'Expected a string. Got: %s',
+                    static::typeToString($arguments[0])
+                ));
+            }
+        }
+
+    }
+}
 
 // Curl
 // https://github.com/php-curl-class/php-curl-class
-use \Curl\Curl;
-$curl = new Curl();
+$curl = new \Curl\Curl;
 
-// Medoo
-// https://medoo.in/
-use Medoo\Medoo;
-
+// // Medoo
+// // https://medoo.in/
 // try {
-//     $db = new Medoo([
+//     $db = new Medoo\Medoo([
 //         'database_type' => 'mysql',
 //         'database_name' => 'root',
 //         'server' => '127.0.0.1',
